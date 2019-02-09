f92cf7d9ab98b128c1cbbcd51b138ed407701ddd
saras <sarasdfg98@gmail.com>
2019-02-09 13:57:23 +0100

    Grid and player drawing test

--- drawing.py
+++ drawing.py
@@ -7,7 +7,7 @@ width = 510
 height = 510
 
 field_size = 100
-circle_size = 70
+circle_size = 40
 
 screen = pygame.display.set_mode((width, height))
 done = False
@@ -20,14 +20,26 @@ while not done:
 
     screen.fill((0, 0, 0))
 
+    board = np.array([['_', '_', '_', ' ☀', '_'], ['_', ' ❄️', '_', '_', '_'], ['_', ' ❄️', '_', ' ☀', '_'], ['_', '_', '_', '_', '_'], [' ❄️', '_', '_', '_', '_']])
+
     # draws grid
-    for h in range(height):
-        for w in range(width):
-            rect = pygame.Rect(w * (field_size + 2), h * (field_size + 2), 
-                                field_size, field_size)
+    for h in range(int(height/5)):
+        for w in range(int(width/5)):
+            rect = pygame.Rect(w * (field_size + 2), h * (field_size + 2), field_size, field_size)
+
             pygame.draw.rect(screen, (255, 255, 255), rect)
 
-    # draws players
+    # draws player
+    player_i, player_j = np.where(board == ' ❄️')
+
+    for p in range(0, len(player_i)):
+        player_circle = pygame.draw.circle(screen, (235, 55, 10), [player_j[p] * 102 + 51, player_i[p] * 102 + 51], circle_size)
+
+    # draws algorithm
+    algorithm_i, algorithm_j = np.where(board == ' ☀')
+
+    for a in range(0, len(algorithm_i)):
+        algorithm_circle = pygame.draw.circle(screen, (52, 55, 10), [algorithm_j[a] * 102 + 51, algorithm_i[a] * 102 + 51], circle_size)
 
     pygame.display.flip()
 
